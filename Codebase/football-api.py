e23a4a1f9f073bcf5754527ad1a8695fd286abfc
Angelo <angelo.pennati@quantilope.com>
2022-05-19 14:35:30 +0200

    Football API Addition

---
+++ Codebase/football-api.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import json
+import pandas as pd
+import requests
+
+
+
+
+
+key = '7e09245ac8mshf3d314f186c5081p1031b8jsn87ef9651678f'
+last_name = 'tonali'
+
+def player_search(
+    last_name : str = None,
+    api_key : str = None
+    ):
+
+    url = "https://api-football-v1.p.rapidapi.com/v2/players/search/{last_name}"
+    headers = {
+        'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+        'x-rapidapi-key': key
+        }
+    
+    rsp = requests.request("GET", url, headers = headers)
+    return rsp.text
+
+trial = player_search(last_name = last_name, api_key = key)
+
+print(trial)
--- Codebase/tomori_trial.py
+++ Codebase/tomori_trial.py
@@ -3,7 +3,7 @@ import requests
 import json
 import pandas as pd
 
-token = "your_token_here"
+token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjZTFhM2U3MS0wOWJlLTRlZWMtYjU2MC1lZGUwOGVlNmY5ZTMiLCJzY3AiOiJ1c2VyIiwiYXVkIjoiYXBwIiwiaWF0IjoxNjUxMDc4NTU3LCJleHAiOiIxNjgyNjM1NTA5IiwianRpIjoiZjBlNWE1NWEtNjkxYi00ODQwLTgzOGUtMjAyOTQ2YWNjMDJiIn0._v_uv3MB4rxasORoJge7NJ8tlBWqXGLwuT9y-941nOg"
 headers = {"content-type" : "application/json", "Authorization" : token, "JWT-AUD": 'Cost Analysis'}
 
 def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
@@ -22,6 +22,7 @@ query{
       nodes{
         name
         assetId
+        canBuy
         slug
         rarity
         serialNumber
